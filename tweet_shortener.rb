1f904b302e3814822ae344c9f61fc8dc3f7f7c21
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-06 04:26:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,26 +19,20 @@ def word_substituter(tweet)
         word.replace(dictionary[word])
       else
         word
-      end 
+      end  
     end.join(" ")
 end
 
-# def bulk_tweet_shortener(tweets)
-#   tweets.split(" ").map do |phrase|
-#     puts word_substituter(phrase)
-#   end
-# end
-
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
+def bulk_tweet_shortener(tweets)
+  tweets.map do |phrase|
     puts word_substituter(phrase)
   end
 end
 
 def selective_tweet_shortener(tweets)
   tweets.split(" ").map do |phrase|
-    if phrase.length <= 140
-      phrase
+    if phrase.length > 140
+      word_substituter(phrase)
     else
       word_substituter(tweet)
     end
