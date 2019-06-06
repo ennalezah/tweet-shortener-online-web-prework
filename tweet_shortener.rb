abb4d8e0ece844c229ec77dfcacc433be662b711
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-06 05:07:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -30,7 +30,11 @@ def bulk_tweet_shortener(arr_tweets)
 end 
 
 def selective_tweet_shortener(tweet)
-  if tweet.
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
 end
 
 # def shortened_tweet_truncator(tweet)
