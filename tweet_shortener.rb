7dc2be159f941aabc06be9ca44f7901aced67788
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-06 03:27:01 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,59 @@
-# Write your code here.
+def dictionary
+  convert = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+# def word_substituter(tweet)
+#   tweet.split(//).each do |word|
+#     if dictionary.keys.include?(word.downcase)
+#       # word = dictionary[word]
+#       tweet.map do |word|
+#         word.gsub(word, dictionary[word])
+#       end 
+#     end 
+#   end
+# end
+
+# def word_substituter(tweet)
+#   tweet.gsub(word, dictionary[word])
+# end
+
+# def word_substituter(tweet)
+#     tweet.split(" ").map do |word|
+#       dictionary.each do |k, v|
+#         if word === k
+          
+#         end 
+#       end 
+        
+#     end
+# end
+
+def word_substituter(tweet)
+    tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word.replace(dictionary[word])
+      end 
+    end.join(" ")
+end
+
+# def word_substituter(tweet)
+#     tweet.split(" ").map do |word|
+#       if dictionary.keys.include?(word.downcase)
+#         tweet.map { |word| word == 4 ? 'Z' : x }
+        
+#         tweet
+#       end 
+#     end
+#     tweet.join(" ")
+# end
